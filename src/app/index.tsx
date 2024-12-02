56208511dcfeae4db306b5fb630a3ef1190750bf
DESKTOP-UKL8HD9\victo <victor.gomes@redefrota.com.br>
2024-12-01 21:49:44 -0300

    Config: Initial commit.

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="h-full w-full items-center justify-center">
+      <Text className="text-red-900 text-xl">Edited app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
